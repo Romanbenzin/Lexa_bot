01b52ecde04df9d079d92649528a4a7d108e9823
roman.shchetnikov <mmavrr@mail.ru>
2024-09-18 11:48:52 +0300

    Создание леха бота

---
+++ bot.py
@@ -0,0 +1,16 @@
+import telebot
+bot = telebot.TeleBot('7385255905:AAHYbZdHtqUMqiaiX-ZnWOoTNgCJrZGRREg')
+
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+def handle_text(message):
+    if "LEXA DOTA" in message.text.upper():
+        question = 'в дотку пидоры?'
+        options = ['да', 'пизда', 'леха', 'нет, я гей']
+        poll = bot.send_poll(message.chat.id, question, options, is_anonymous=False)
+        print(poll)
+    elif "LEXA" in message.text.upper():
+        bot.send_message(message.chat.id, "Привет, вы вызвали LEXY зомби")
+    else:
+        pass
+
+bot.polling(none_stop=True, interval=0)
