e0ad7ac1c5a0225612dfed767686969d10c8a7c4
Romanbenzin <mmavrr@mail.ru>
2024-09-18 15:19:06 +0300

    ss

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ bot = telebot.TeleBot('7385255905:AAHYbZdHtqUMqiaiX-ZnWOoTNgCJrZGRREg')
 
 @bot.message_handler(content_types=['text', 'document', 'audio'])
 def handle_text(message):
-    if "LEXA DOTA" in message.text.upper():
+    if "/DOTA" in message.text.upper():
         question = 'в дотку пидоры?'
         options = ['да', 'пизда', 'леха', 'нет, я гей']
         poll = bot.send_poll(message.chat.id, question, options, is_anonymous=False)
         print(poll)
-    elif "LEXA" in message.text.upper():
+    elif "/LEXA" in message.text.upper():
         bot.send_message(message.chat.id, "Привет, вы вызвали LEXY зомби")
     elif "TATAR" in message.text.upper():
         bot.send_message(message.chat.id, "@hqdicq хотел бы мне отсосать? (я леха)")
