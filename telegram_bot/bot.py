70365ac4d467e1d16ab9024dc4fd0cde5797a36f
Roman <mmavrr@mail.ru>
2025-02-16 01:55:03 +0300

    refactor

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,11 +1,5 @@
-import requests
 import pass_bot
-import random
-import re
 from telebot import types
-from deepseek.requests_to_deepseek import api_request
-from team_speak.team_speak_server import status_server
-from data_base.data_base_actions import game_get
 from telegram_bot.bot_core.bot_core import Handler
 from telegram_bot.bot_database_core.bot_database_core import DbHandler
 
@@ -71,91 +65,27 @@ def db_game_get(message):
 
 @bot.message_handler(commands=['weather_izh'])
 def weather_izh(message):
-    response = requests.get('https://api.open-meteo.com/v1/forecast?latitude=56.8498&longitude=53.2045&hourly=temperature_2m&forecast_days=1')
-    list_of_temperature = response.json()['hourly']['temperature_2m']
-    bot.send_message(message.chat.id, f"Минимальная температура сегодня в Ижевске : {min(list_of_temperature)}")
-    bot.send_message(message.chat.id, f"Максимальная температура сегодня в Ижевске : {max(list_of_temperature)}")
+    handler.handle_weather(message)
 
 @bot.message_handler(func=lambda message: message.text and message.text.startswith('/i'))
 def request_to_ii(message):
-    try:
-        # Лог для отладки
-        print(f"Получено сообщение: {message.text}")
-
-        # Извлекаем текст запроса из команды
-        command_parts = message.text.split(maxsplit=1)  # Разделяем команду и текст запроса
-        if len(command_parts) < 2:
-            bot.send_message(message.chat.id, "Пожалуйста, укажите текст запроса после команды /i")
-            return
-
-        user_request = command_parts[1].strip()  # Текст запроса
-        if not user_request:
-            bot.send_message(message.chat.id, "Запрос не может быть пустым.")
-            return
-
-        # Формируем запрос к DeepSeek
-        response = api_request(user_request)
-
-        # Обработка ответа
-        if response.status_code == 200:
-            answer = response.json()["choices"][0]["message"]["content"]
-            bot.send_message(message.chat.id, answer)
-        else:
-            bot.send_message(message.chat.id, f"Ошибка при запросе к API: {response.status_code}")
-
-        print("Запрос:", message.text)
-        print("Ответ:", response.status_code, response.text)
-
-    except Exception as e:
-        # Логируем ошибку
-        print(f"Ошибка: {e}")
-        bot.send_message(message.chat.id, "Произошла ошибка при обработке запроса.")
+    handler.handle_ii(message)
 
 @bot.message_handler(func=lambda message: message.text and message.text.startswith('/roll-'))
 def roll_with_arg(message):
-    try:
-        # Лог для отладки
-        print(f"Получено сообщение: {message.text}")
-
-        # Извлекаем значение n из команды
-        command_parts = message.text.split('-')
-        if len(command_parts) != 2 or not command_parts[1].isdigit():
-            bot.send_message(message.chat.id,
-                             "Пожалуйста, используйте команду в формате /roll-n, где n - максимальное число.")
-            return
-
-        n = int(command_parts[1])
-        if n < 1:
-            bot.send_message(message.chat.id, "Число n должно быть больше 0.")
-            return
-
-        # Генерируем случайное число
-        number = random.randint(1, n)
-        bot.send_message(message.chat.id, f"Вы выбросили число: {number}")
-    except Exception as e:
-        print(f"Ошибка: {e}")  # Логирование ошибки
-        bot.send_message(message.chat.id, f"Произошла ошибка: {e}")
+    handler.handle_roll(message)
 
 @bot.message_handler(commands=['roll'])
 def roll(message):
-    bot.send_message(message.chat.id, "Пожалуйста, используйте команду в формате /roll-n, где n - максимальное число.")
+    handler.handle_just_roll(message)
 
 @bot.message_handler(commands=['teamspeak_status'])
 def teamspeak_status(message):
-    response = status_server()
-    bot.send_message(message.chat.id, f"Статус сервера teamspeak: {response}")
+    handler.handle_team_speak_status(message)
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    user = message.from_user
-    chat_id = message.chat.id
-    try:
-        if message.text.lower() == 'привет леха!':
-            bot.send_message(chat_id, f"Привет, @{user.username}!")
-        else:
-            pass
-    except:
-        pass
+    handler.handle_message_from_user(message)
 
 bot.set_my_commands([
     types.BotCommand("start",   "Запустить бота"),
--- telegram_bot/bot_core/bot_core.py
+++ telegram_bot/bot_core/bot_core.py
@@ -1,5 +1,8 @@
 import random
+import requests
 
+from team_speak.team_speak_server import status_server
+from deepseek.requests_to_deepseek import api_request
 from telebot import types
 from telegram_bot.pass_bot import users, users_without_yana
 from telegram_bot.helpers import list_formatter
@@ -65,3 +68,87 @@ class Handler:
     def handle_uebishche(self, message):
         user = random.choice(users_without_yana)
         self.bot.send_message(message.chat.id, f"Уебище это: {user}")
+
+    def handle_weather(self, message):
+        response = requests.get(
+            'https://api.open-meteo.com/v1/forecast?latitude=56.8498&longitude=53.2045&hourly=temperature_2m&forecast_days=1')
+        list_of_temperature = response.json()['hourly']['temperature_2m']
+        self.bot.send_message(message.chat.id, f"Минимальная температура сегодня в Ижевске : {min(list_of_temperature)}")
+        self.bot.send_message(message.chat.id, f"Максимальная температура сегодня в Ижевске : {max(list_of_temperature)}")
+
+    def handle_ii(self, message):
+        try:
+            # Лог для отладки
+            print(f"Получено сообщение: {message.text}")
+
+            # Извлекаем текст запроса из команды
+            command_parts = message.text.split(maxsplit=1)  # Разделяем команду и текст запроса
+            if len(command_parts) < 2:
+                self.bot.send_message(message.chat.id, "Пожалуйста, укажите текст запроса после команды /i")
+                return
+
+            user_request = command_parts[1].strip()  # Текст запроса
+            if not user_request:
+                self.bot.send_message(message.chat.id, "Запрос не может быть пустым.")
+                return
+
+            # Формируем запрос к DeepSeek
+            response = api_request(user_request)
+
+            # Обработка ответа
+            if response.status_code == 200:
+                answer = response.json()["choices"][0]["message"]["content"]
+                self.bot.send_message(message.chat.id, answer)
+            else:
+                self.bot.send_message(message.chat.id, f"Ошибка при запросе к API: {response.status_code}")
+
+            print("Запрос:", message.text)
+            print("Ответ:", response.status_code, response.text)
+
+        except Exception as e:
+            # Логируем ошибку
+            print(f"Ошибка: {e}")
+            self.bot.send_message(message.chat.id, "Произошла ошибка при обработке запроса.")
+
+    def handle_roll(self, message):
+        try:
+            # Лог для отладки
+            print(f"Получено сообщение: {message.text}")
+
+            # Извлекаем значение n из команды
+            command_parts = message.text.split('-')
+            if len(command_parts) != 2 or not command_parts[1].isdigit():
+                self.bot.send_message(message.chat.id,
+                                 "Пожалуйста, используйте команду в формате /roll-n, где n - максимальное число.")
+                return
+
+            n = int(command_parts[1])
+            if n < 1:
+                self.bot.send_message(message.chat.id, "Число n должно быть больше 0.")
+                return
+
+            # Генерируем случайное число
+            number = random.randint(1, n)
+            self.bot.send_message(message.chat.id, f"Вы выбросили число: {number}")
+        except Exception as e:
+            print(f"Ошибка: {e}")  # Логирование ошибки
+            self.bot.send_message(message.chat.id, f"Произошла ошибка: {e}")
+
+    def handle_just_roll(self, message):
+        self.bot.send_message(message.chat.id,
+                         "Пожалуйста, используйте команду в формате /roll-n, где n - максимальное число.")
+
+    def handle_team_speak_status(self, message):
+        response = status_server()
+        self.bot.send_message(message.chat.id, f"Статус сервера teamspeak: {response}")
+
+    def handle_message_from_user(self, message):
+        user = message.from_user
+        chat_id = message.chat.id
+        try:
+            if message.text.lower() == 'привет леха!':
+                self.bot.send_message(chat_id, f"Привет, @{user.username}!")
+            else:
+                pass
+        except:
+            pass
